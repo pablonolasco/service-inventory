be466ed03bbe53f65e3792289c8be11937892ef4
Pablo Nolasco Castro <pablo.nolasco@people-media.com.mx>
2022-09-27 09:11:38 -0500

    Feat: se agrego archivos response producto

---
+++ src/main/java/com/company/inventory/response/ProductoResponse.java
@@ -0,0 +1,13 @@
+package com.company.inventory.response;
+
+import java.util.List;
+
+import com.company.inventory.model.ProductoEntity;
+
+import lombok.Data;
+
+@Data
+public class ProductoResponse {
+
+	public List<ProductoEntity>productoEntities;
+}
