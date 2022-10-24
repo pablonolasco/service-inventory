1f05f733b634d4f34c08c99164f0421380a079af
pablo.nolasco <pablo.nolasco@live.com.mx>
2022-10-24 16:32:33 -0500

    Feat: add cross en producto

--- src/main/java/com/company/inventory/controller/ProductoController.java
+++ src/main/java/com/company/inventory/controller/ProductoController.java
@@ -5,12 +5,14 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +24,7 @@ import com.company.inventory.util.UtilImagen;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class ProductoController {
 
 	@Autowired
